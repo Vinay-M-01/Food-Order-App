d5d107f734949517431c2a3f3567e1f579487150
Vinay-M-01 <vinaydm1011@gmail.com>
2022-07-12 15:28:21 +0530

    Cart Open and Close

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
+import MealsSummary from './components/Meals/MealsSummary';
+import AvailableMeals from './components/Meals/AvailableMeals';
 import Cart from './components/Cart/Cart';
 
+
 function App() {
+  const [showCart, setShowCart] = useState(false)
+  const dummyItems = [
+    {
+        dishName:'Sushi',
+        description:'Finest Fish and Vegies',
+        amount:'$29.99',
+        id: Math.random().toString()
+    },
+    {
+        dishName:'Dosa',
+        description:'Dharwad Speciality',
+        amount:'$10.99',
+        id: Math.random().toString()
+    },
+    {
+        dishName:'Idli',
+        description:'Begluru Speciality',
+        amount:'$9.99',
+        id: Math.random().toString()
+    },
+    {
+        dishName:'Biriyani',
+        description:'BVB Speciality',
+        amount:'$15.99',
+        id: Math.random().toString()
+    }
+   
+]
+const doThis = ()=>{
+  
+    setShowCart(true)
+
+}
+const closeTheModal = ()=>{
+  setShowCart(false)
+
+}
   return (
     <Fragment>
-      <Cart/>
-      <Header />
-      <main>
-        <Meals />
-      </main>
+      {showCart && <Cart CloseCart = {closeTheModal}></Cart>}
+      <Header onCartClick={doThis} />
+      <MealsSummary/>
+      <AvailableMeals Items = {dummyItems}/>
+      
     </Fragment>
   );
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,23 +1,23 @@
-
-import classes from './Header.module.css'
-
-
-import MealsImage from '../../assets/meals.jpg'
 import { Fragment } from 'react';
 import HeaderCartButton from './HeaderCartButton';
 
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+
 const Header = (props) => {
-    return ( 
-        <Fragment>
-            <header className={classes.header}>
-                <h1>React Meals</h1>
-                <HeaderCartButton/>
-            </header>
-            <div className={classes['main-image']}>
-                <img src={MealsImage} alt="A Table full of crap" />
-            </div>
-        </Fragment>
-    );
-}
- 
+   
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onHeaderButtonClick={props.onCartClick}/>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
+
 export default Header;
